30e0a9e8c5d0791e3f6e72bd227a4cd26f1a8f84
Leesin0222 <dragon02229@naver.com>
2021-11-22 22:26:41 +0900

    📑 :: LoginActivity에 뷰바인딩 추가

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/ui/LoginActivity.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/ui/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.yongjincompany.yaho2.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yongjincompany.yaho2.R
+import com.yongjincompany.yaho2.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityLoginBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
