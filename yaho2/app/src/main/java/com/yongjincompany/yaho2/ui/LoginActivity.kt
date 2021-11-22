bde3c3745aea0861817402ac7e7ffcbf8dbb98ad
Leesin0222 <dragon02229@naver.com>
2021-11-22 22:28:19 +0900

    📑 :: 계정이 없음을 클릭 시 회원가입 페이지로 넘어가게함

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/ui/LoginActivity.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/ui/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.yongjincompany.yaho2.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yongjincompany.yaho2.R
@@ -8,10 +9,14 @@ import com.yongjincompany.yaho2.databinding.ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var  binding: ActivityLoginBinding
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.noAccount.setOnClickListener {
+            startActivity(Intent(this,RegisterActivity::class.java))
+        }
     }
 }
