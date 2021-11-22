c0b7902173d684b3497dc58eb63790a38662f014
Leesin0222 <dragon02229@naver.com>
2021-11-22 21:58:02 +0900

    ➕ :: 로그인 회원가입 Activity,xml 파일 생성

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/ui/MainActivity.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yongjincompany.yaho2
+package com.yongjincompany.yaho2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yongjincompany.yaho2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
