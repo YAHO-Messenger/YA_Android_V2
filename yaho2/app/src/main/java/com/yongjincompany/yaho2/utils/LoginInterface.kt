9c7be4c2eef656318868e73ebce6dee1bba29409
Leesin0222 <dragon02229@naver.com>
2021-11-22 22:15:30 +0900

    📑 :: 로그인 인터페이스 완성 (@Post)

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/LoginInterface.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/LoginInterface.kt
@@ -1,4 +1,11 @@
 package com.yongjincompany.yaho2.utils
 
+import com.yongjincompany.yaho2.data.Login
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface LoginInterface {
+    @POST("/api/auth/signup")
+    fun login(@Body login: Login) : Call<String>
 }
