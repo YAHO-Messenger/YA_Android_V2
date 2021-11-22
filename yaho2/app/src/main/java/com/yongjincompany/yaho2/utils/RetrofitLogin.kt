06c2e94c841197f22f6ee237d7a346d65b454e68
Leesin0222 <dragon02229@naver.com>
2021-11-22 22:18:17 +0900

    📑 :: 레트로핏 로그인 오브젝트 완성!

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/RetrofitLogin.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/RetrofitLogin.kt
@@ -1,7 +1,18 @@
 package com.yongjincompany.yaho2.utils
 
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 object RetrofitLogin {
+
+    var gson = GsonBuilder().setLenient().create()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl("http://15.165.28.181:8080")
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+    val service = retrofit.create(LoginInterface::class.java)
 
 }
