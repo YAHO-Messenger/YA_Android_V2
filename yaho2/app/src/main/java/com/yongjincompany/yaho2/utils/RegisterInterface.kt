7cad1e7e0cdbd5ddd8e6b9d65b69c92f3f9537ab
Leesin0222 <dragon02229@naver.com>
2021-11-23 23:24:31 +0900

    📑 :: Register Interface에 로그인 레지스터로 변경

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/RegisterInterface.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/RegisterInterface.kt
@@ -1,11 +1,12 @@
 package com.yongjincompany.yaho2.utils
 
-import com.yongjincompany.yaho2.data.Login
+
+import com.yongjincompany.yaho2.data.Register
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface RegisterInterface {
     @POST("/api/auth/signin")
-    fun register(@Body login: Login) : Call<String>
+    fun register(@Body register: Register) : Call<String>
 }
