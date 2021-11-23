cbb21355c5271a84e3351098a609c32bc35f0066
Leesin0222 <dragon02229@naver.com>
2021-11-23 23:25:53 +0900

    📑 :: RetrofitLogin과 겹치는 부분 수정

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/RetrofitRegister.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/RetrofitRegister.kt
@@ -1,6 +1,7 @@
 package com.yongjincompany.yaho2.utils
 
 import com.google.gson.GsonBuilder
+import com.yongjincompany.yaho2.utils.RetrofitLogin.retrofit
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
@@ -9,10 +10,10 @@ object RetrofitRegister {
 
     var gson = GsonBuilder().setLenient().create()
 
-    val retrofit = Retrofit.Builder()
+    val retrofit2 = Retrofit.Builder()
         .baseUrl("http://15.165.28.181:8080")
         .addConverterFactory(ScalarsConverterFactory.create())
         .addConverterFactory(GsonConverterFactory.create(gson))
         .build()
-    val service = retrofit.create(RegisterInterface::class.java)
+    val service2 = retrofit2.create(RegisterInterface::class.java)
 }
