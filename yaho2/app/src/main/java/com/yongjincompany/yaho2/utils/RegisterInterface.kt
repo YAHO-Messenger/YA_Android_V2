2b7f99f69f40734d58f53de5130ef3bf4bcf71ae
Leesin0222 <dragon02229@naver.com>
2021-11-23 22:31:20 +0900

    📑 :: @POST << @Body login:Login

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/RegisterInterface.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/RegisterInterface.kt
@@ -1,4 +1,11 @@
 package com.yongjincompany.yaho2.utils
 
+import com.yongjincompany.yaho2.data.Login
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface RegisterInterface {
+    @POST("/api/auth/signin")
+    fun register(@Body login: Login) : Call<String>
 }
