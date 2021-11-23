dcd41a87b33f6485bad3778e9d666c7792f983cd
Leesin0222 <dragon02229@naver.com>
2021-11-23 23:28:45 +0900

    📑 :: 회원가입 구현 완.

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/ui/RegisterActivity.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/ui/RegisterActivity.kt
@@ -1,12 +1,42 @@
 package com.yongjincompany.yaho2.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.yongjincompany.yaho2.R
+import android.util.Log
+import com.yongjincompany.yaho2.data.Register
+import com.yongjincompany.yaho2.databinding.ActivityRegisterBinding
+import com.yongjincompany.yaho2.utils.RetrofitRegister
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.haveAccount.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
+        binding.registerBtn.setOnClickListener {
+
+            val username = binding.userEt2.toString()
+            val email = binding.idEt2.toString()
+            val password = binding.passwordEt2.toString()
+            RetrofitRegister.service2.register(Register(username, email, password)).enqueue(object : Callback<String> {
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    val result = response.body()
+                    Log.e("로그인", "${result}")
+                }
+
+                override fun onFailure(call: Call<String>, t:Throwable) {
+                    Log.e("로그인", "${t.localizedMessage}")
+                }
+            })
+        }
     }
 }
