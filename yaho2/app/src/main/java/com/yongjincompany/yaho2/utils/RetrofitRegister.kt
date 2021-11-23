852ca679d5b80265b71336feaad860fff51afea2
Leesin0222 <dragon02229@naver.com>
2021-11-23 22:33:39 +0900

    📑 :: 레트로핏 회원가입 오브젝트 완성!

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/RetrofitRegister.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/RetrofitRegister.kt
@@ -1,4 +1,18 @@
 package com.yongjincompany.yaho2.utils
 
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
 object RetrofitRegister {
+
+    var gson = GsonBuilder().setLenient().create()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl("http://15.165.28.181:8080")
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+    val service = retrofit.create(RegisterInterface::class.java)
 }
