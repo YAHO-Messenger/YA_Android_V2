09b8fc0501fd1571eb0da78376a0c526c224587d
Leesin0222 <dragon02229@naver.com>
2021-11-25 17:18:48 +0900

    📑 :: MainActivity, activity_main을 Chat으로 이름 변경

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/ui/ChatActivity.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/ui/ChatActivity.kt
@@ -3,10 +3,14 @@ package com.yongjincompany.yaho2.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yongjincompany.yaho2.R
+import com.yongjincompany.yaho2.databinding.ActivityChatBinding
 
-class MainActivity : AppCompatActivity() {
+class ChatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChatBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
 }
