8dd87adeed348979b78e6aec35899c13f5ac2e59
Leesin0222 <dragon02229@naver.com>
2021-11-27 13:48:35 +0900

    📑:: RoomInterface  @post추가ㅓ

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/RoomInterface.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/RoomInterface.kt
@@ -1,4 +1,12 @@
 package com.yongjincompany.yaho2.utils
 
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface RoomInterface {
+    @FormUrlEncoded
+    @POST("/api/auth/chat")
+    fun made_room(@Field ("roomname") name: String) : Call<String>
 }
