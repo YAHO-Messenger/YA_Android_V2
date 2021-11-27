9cecffa272345deff7fe0f06b2482eae90dc8bd1
Leesin0222 <dragon02229@naver.com>
2021-11-27 12:02:54 +0900

    ➕:: MadeRoomActivity , xml 파일 생성

---
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/ui/MadeRoomActivity.kt
@@ -0,0 +1,11 @@
+package com.yongjincompany.yaho2.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MadeRoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_made_room)
+    }
+}
