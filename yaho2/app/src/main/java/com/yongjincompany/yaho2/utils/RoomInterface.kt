07e81b80a16475f688d87171182256e79fa063b6
Leesin0222 <dragon02229@naver.com>
2021-11-30 16:42:56 +0900

    📑 :: 함수 이름 room으로 변경

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/RoomInterface.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/RoomInterface.kt
@@ -3,10 +3,11 @@ package com.yongjincompany.yaho2.utils
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface RoomInterface {
     @FormUrlEncoded
     @POST("/api/auth/chat")
-    fun made_room(@Field ("roomname") name: String) : Call<String>
+    fun room(@Field ("roomname") name: String) : Call<String>
 }
