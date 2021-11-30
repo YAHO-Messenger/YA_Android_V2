9c59f4dd3f92eb6832b2696156dc51ee45ad6da1
Leesin0222 <dragon02229@naver.com>
2021-11-30 19:02:24 +0900

    📑:: @GET으로 받음

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/GetRoomInterface.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/utils/GetRoomInterface.kt
@@ -1,4 +1,11 @@
 package com.yongjincompany.yaho2.utils
 
+import com.yongjincompany.yaho2.data.Room
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface GetRoomInterface {
+
+    @GET("/api/auth/chat")
+    suspend fun getRooms(): Response<List<Room>>
 }
