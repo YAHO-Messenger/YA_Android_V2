ddacb80cafc1191610aed80e66001c2b9f7149e5
Leesin0222 <dragon02229@naver.com>
2021-11-30 20:14:51 +0900

    📑:: 채팅방 리스트 불러오기 성공 하지만.. 방 생성 아직 실패

--- yaho2/app/src/main/java/com/yongjincompany/yaho2/ui/RoomActivity.kt
+++ yaho2/app/src/main/java/com/yongjincompany/yaho2/ui/RoomActivity.kt
@@ -1,6 +1,7 @@
 package com.yongjincompany.yaho2.ui
 
 import android.app.AlertDialog
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -8,19 +9,27 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yongjincompany.yaho2.R
 import com.yongjincompany.yaho2.data.Login
 import com.yongjincompany.yaho2.data.Room
 import com.yongjincompany.yaho2.databinding.ActivityRoomBinding
 import com.yongjincompany.yaho2.utils.RetrofitLogin
 import com.yongjincompany.yaho2.utils.RetrofitRoom
+import com.yongjincompany.yaho2.utils.RetrofitRoomGet
 import com.yongjincompany.yaho2.utils.RoomAdapter
 import kotlinx.android.synthetic.main.dialog_view.*
 import kotlinx.android.synthetic.main.dialog_view.view.*
 import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.HttpException
 import retrofit2.Response
+import java.io.IOException
 
+
+const val TAG = "RoomActivity"
 class RoomActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityRoomBinding
@@ -31,6 +40,31 @@ class RoomActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRoomBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupRecyclerView()
+
+        lifecycleScope.launchWhenCreated {
+            binding.progressBar.isVisible = true
+            val response = try {
+                RetrofitRoomGet.service4.getRooms()
+            } catch (e: IOException) {
+                Log.e(TAG, "IOException, 인터넷연결 해라")
+                binding.progressBar.isVisible = false
+                return@launchWhenCreated
+            } catch (e: HttpException) {
+                Log.e(TAG, "HttpException, 응답이 어디갔을까요?")
+                return@launchWhenCreated
+            }
+            if (response.isSuccessful && response.body() != null) {
+                roomAdapter.rooms = response.body()!!
+            } else {
+                Log.e(TAG, "Responce가 실패합니다")
+            }
+            binding.progressBar.isVisible = false
+        }
+
+
+
+
 
 
         binding.madeRoomBtn.setOnClickListener {
@@ -47,7 +81,7 @@ class RoomActivity : AppCompatActivity() {
             view.made_room_dialog.setOnClickListener {
 
 
-                RetrofitRoom.service3.room("room").enqueue(object : Callback<String> {
+                RetrofitRoom.service3.room("xfsfdf").enqueue(object : Callback<String> {
                     override fun onResponse(call: Call<String>, response: Response<String>) {
                         val result = response.body()
                         Log.e("방 생성", "${result}")
@@ -67,5 +101,10 @@ class RoomActivity : AppCompatActivity() {
 
             }
         }
+    }
+    private fun setupRecyclerView() = binding.list.apply {
+        roomAdapter = RoomAdapter()
+        adapter = roomAdapter
+        layoutManager = LinearLayoutManager(this@RoomActivity)
     }
 }
